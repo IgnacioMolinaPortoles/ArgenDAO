55f4e271805766c97611d65eb58f2407a3674ac7
Ignacio Nicanor Sebastian Molina Portoles <imolina@intermediait.com>
2021-12-20 15:17:38 -0300

    c n

--- src/App.jsx
+++ src/App.jsx
@@ -179,7 +179,7 @@ const App = () => {
   if (!address) {
     return (
       <div className="landing">
-        <h1>Welcome to NarutoDAO</h1>
+        <h1>Welcome to ArgenDAO</h1>
         <button onClick={() => connectWallet("injected")} className="btn-hero">
           Connect your wallet
         </button>
